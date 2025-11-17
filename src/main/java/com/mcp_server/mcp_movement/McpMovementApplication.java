a29aeb1f783e8fe04a357ebaaee5d4951e053fe9
mati <mferrerovilas@frba.utn.edu.ar>
2025-11-16 22:16:18 -0300

    initial

---
+++ src/main/java/com/mcp_server/mcp_movement/McpMovementApplication.java
@@ -0,0 +1,13 @@
+package com.mcp_server.mcp_movement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpMovementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpMovementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcp_server/mcp_movement/McpMovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp_server.mcp_movement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpMovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
